7460fb71944444eab5465df9164008483b8d5161
iluvshiwoon <sntcillian@gmail.com>
2025-09-03 17:18:45 +0200

    ex 02 done

--- C++01/ex02/main.cpp
+++ C++01/ex02/main.cpp
@@ -6,6 +6,13 @@ int main(){
     std::string* stringPTR = &string;
     std::string& stringREF = string;
 
+    std::cout << "Memory address of the string variable: " << &string << std::endl;
+    std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl; 
+    std::cout << "Memory address held by stringREF: " << &stringREF << std::endl; 
+    std::cout << std::endl << "Value of the string variable: " << string << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+
 
 
     return 1;
