9708e3dad847fb52271522827da656b3f229a067
iluvshiwoon <sntcillian@gmail.com>
2025-12-21 09:39:47 -0500

    do ex02

---
+++ C++08/ex02/MutantStack.hpp
@@ -0,0 +1,18 @@
+#ifndef MUTANT_STACK_HPP
+#define MUTANT_STACK_HPP
+#include <stack>
+template <typename T> class MutantStack : public std::stack<T> {
+private:
+public:
+  MutantStack() : std::stack<T>() {};
+  MutantStack(const MutantStack &other) : std::stack<T>(other) {};
+  MutantStack &operator=(const MutantStack &rhs) {
+    if (this != &rhs)
+      std::stack<T>::operator=(rhs);
+  };
+  virtual ~MutantStack() {};
+  typedef typename std::stack<T>::container_type::iterator iterator;
+  iterator begin() { return this->c.begin(); }
+  iterator end() { return this->c.end(); }
+};
+#endif
