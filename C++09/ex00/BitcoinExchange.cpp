f8dd2651af4fec93aa2eaefd1a22b4d2f0137083
iluvshiwoon <sntcillian@gmail.com>
2026-03-24 14:33:45 +0100

    fix: cleanup

--- C++09/ex00/BitcoinExchange.cpp
+++ C++09/ex00/BitcoinExchange.cpp
@@ -11,9 +11,6 @@
 BitcoinExchange::BitcoinExchange(const std::string &filename) {
   _parse_file("data.csv", DB, -1.0f, ",");
   _parse_file(filename, INPUT, 1000.0f, " | ");
-
-  // _load_database("data.csv");
-  // _execute(filename);
 }
 
 BitcoinExchange::~BitcoinExchange() {};
@@ -59,12 +56,12 @@ bool BitcoinExchange::_check_date(int n, const std::string &date,
 
 bool BitcoinExchange::_check_value(int n, const std::string &value_s,
                                    float &value_f, const std::string &filename,
-                                   const float &limit) {
+                                   const float &limit, int mode) {
   std::istringstream ss(value_s);
   char trailing;
 
   if (ss >> std::noskipws >> value_f && !(ss >> trailing) && value_f >= 0 &&
-      (limit == -1 || value_f <= limit))
+      (mode == DB || value_f <= limit))
     return true;
 
   std::cerr << "line " << n << " in " << filename << " : Invalid value\n";
@@ -79,80 +76,6 @@ bool _is_open_file(const std::string &filename, std::ifstream &istrm) {
   return true;
 }
 
-void BitcoinExchange::_load_database(const std::string &filename) {
-  std::ifstream istrm(filename.c_str());
-
-  if (!_is_open_file(filename, istrm))
-    return;
-
-  std::string line;
-  std::string date;
-  std::string value_s;
-  std::string::size_type i;
-  float value_f;
-  int n = 0;
-  while (std::getline(istrm, line)) {
-    if (n++ == 0)
-      continue;
-
-    i = line.find(',');
-    if (i == std::string::npos) {
-      std::cerr << "line " << n << " in " << filename << " : wrong format\n";
-      continue;
-    }
-    date = line.substr(0, i);
-    value_s = line.substr(i + 1);
-    if (_check_date(n, date, filename) &&
-        _check_value(n, value_s, value_f, filename, -1.0f))
-      _database[date] = value_f;
-  }
-  if (n <= 1)
-    throw std::runtime_error("Invalid csv");
-}
-
-float BitcoinExchange::_get_rate(const std::string &date) {
-  std::map<std::string, float>::iterator it = this->_database.lower_bound(date);
-  if (it == this->_database.begin() && it->first != date)
-    throw std::out_of_range(" : rates out of range\n");
-  if (it != this->_database.end() && it->first == date)
-    return it->second;
-  --it;
-  return it->second;
-}
-
-void BitcoinExchange::_execute(const std::string &filename) {
-  std::ifstream istrm(filename.c_str());
-
-  if (!_is_open_file(filename, istrm))
-    return;
-
-  std::string line;
-  std::string date;
-  std::string value_s;
-  std::string::size_type i;
-  float value_f;
-  int n = 0;
-  while (std::getline(istrm, line)) {
-    if (n++ == 0)
-      continue;
-    i = line.find(" | ");
-    if (i == std::string::npos) {
-      std::cerr << "line " << n << " in " << filename << " : wrong format\n";
-      continue;
-    }
-    date = line.substr(0, i);
-    value_s = line.substr(i + 3);
-    if (_check_date(n, date, filename) &&
-        _check_value(n, value_s, value_f, filename, 1000.0f)) {
-      try {
-        float result = _get_rate(date) * value_f;
-        std::cout << date << " => " << value_f << " = " << result << "\n";
-      } catch (std::exception &e) {
-        std::cerr << "line " << n << " in " << filename << e.what();
-      }
-    }
-  }
-}
 void BitcoinExchange::_parse_file(const std::string &filename, int mode,
                                   const float &limit,
                                   const std::string &pattern) {
@@ -178,7 +101,7 @@ void BitcoinExchange::_parse_file(const std::string &filename, int mode,
     date = line.substr(0, i);
     value_s = line.substr(i + pattern.length());
     if (_check_date(n, date, filename) &&
-        _check_value(n, value_s, value_f, filename, limit)) {
+        _check_value(n, value_s, value_f, filename, limit, mode)) {
       if (mode == DB)
         _database[date] = value_f;
       else if (mode == INPUT) {
@@ -193,4 +116,13 @@ void BitcoinExchange::_parse_file(const std::string &filename, int mode,
   }
   if (mode == DB && n <= 1)
     throw std::runtime_error("Invalid csv");
+}
+float BitcoinExchange::_get_rate(const std::string &date) {
+  std::map<std::string, float>::iterator it = this->_database.lower_bound(date);
+  if (it == this->_database.begin() && it->first != date)
+    throw std::out_of_range(" : rates out of range\n");
+  if (it != this->_database.end() && it->first == date)
+    return it->second;
+  --it;
+  return it->second;
 }
--- C++09/ex00/BitcoinExchange.hpp
+++ C++09/ex00/BitcoinExchange.hpp
@@ -6,12 +6,10 @@ class BitcoinExchange {
 private:
   enum _mode { DB, INPUT };
   std::map<std::string, float> _database;
-  void _load_database(const std::string &filename);
   bool _check_date(int n, const std::string &date, const std::string &filename);
   bool _check_value(int n, const std::string &value_s, float &value_f,
-                    const std::string &filename, const float &limit);
+                    const std::string &filename, const float &limit, int mode);
   float _get_rate(const std::string &date);
-  void _execute(const std::string &filename);
   void _parse_file(const std::string &filename, int mode, const float &limit,
                    const std::string &pattern);
 
