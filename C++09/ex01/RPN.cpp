62265d3cd11ec03066725f41846d54247d593469
iluvshiwoon <sntcillian@gmail.com>
2026-03-30 00:22:40 +0200

    feat: fix transient state

--- C++09/ex01/RPN.cpp
+++ C++09/ex01/RPN.cpp
@@ -6,29 +6,29 @@
 #include <stdexcept>
 #include <string>
 
-void RPN::_process_ops(const std::string &token) {
-  if (this->_tokens.size() < 2) {
+void RPN::_process_ops(std::stack<int> &tokens, const std::string &token) {
+  if (tokens.size() < 2) {
     std::cerr << "unexpected token : ";
     throw std::runtime_error(token + "\n");
   }
   char op = token[0];
-  const int a = this->_tokens.top();
+  const int a = tokens.top();
   // std::cout << "A: " << a << std::endl;
-  this->_tokens.pop();
-  const int b = this->_tokens.top();
+  tokens.pop();
+  const int b = tokens.top();
   // std::cout << "B: " << b << std::endl;
-  this->_tokens.pop();
+  tokens.pop();
   if (op == '+')
-    this->_tokens.push(a + b);
+    tokens.push(a + b);
   else if (op == '-')
-    this->_tokens.push(b - a);
+    tokens.push(b - a);
   else if (op == '/') {
     if (a == 0)
       throw std::runtime_error("tried to divide by 0\n");
-    this->_tokens.push(b / a);
+    tokens.push(b / a);
   } else if (op == '*')
-    this->_tokens.push(a * b);
-  // std::cout << "After: " << token << " : " << this->_tokens.top() <<
+    tokens.push(a * b);
+  // std::cout << "After: " << token << " : " << tokens.top() <<
   // std::endl;
 }
 
@@ -38,7 +38,7 @@ void RPN::evaluate(const std::string &expr) {
   std::string token;
   std::string valid_ops("+-/*");
   bool is_ops;
-  this->_tokens = std::stack<int>();
+  std::stack<int> tokens;
   while (ss >> token) {
     is_ops = (valid_ops.find(token) != std::string::npos);
     if (token.length() != 1 || (!std::isdigit(token[0]) && is_ops == false)) {
@@ -46,23 +46,15 @@ void RPN::evaluate(const std::string &expr) {
       throw std::runtime_error(" : wrong token in expr\n");
     }
     if (is_ops == true)
-      this->_process_ops(token);
+      this->_process_ops(tokens, token);
     else
-      this->_tokens.push(std::atoi(token.c_str()));
+      tokens.push(std::atoi(token.c_str()));
   }
-  if (this->_tokens.size() != 1)
+  if (tokens.size() != 1)
     throw std::runtime_error("invalid RPN expression\n");
-  std::cout << this->_tokens.top() << std::endl;
+  std::cout << tokens.top() << std::endl;
 }
 
 RPN::RPN() {}
 
 RPN::~RPN() {}
-
-RPN::RPN(const RPN &other) : _tokens(other._tokens) {}
-
-RPN &RPN::operator=(const RPN &rhs) {
-  if (this != &rhs)
-    this->_tokens = rhs._tokens;
-  return *this;
-}
--- C++09/ex01/RPN.hpp
+++ C++09/ex01/RPN.hpp
@@ -4,13 +4,10 @@
 #include <string>
 class RPN {
 private:
-  std::stack<int> _tokens;
-  void _process_ops(const std::string &token);
+  void _process_ops(std::stack<int> &tokens, const std::string &token);
 
 public:
   RPN();
-  RPN(const RPN &other);
-  RPN &operator=(const RPN &rhs);
   ~RPN();
 
   void evaluate(const std::string &expr);
